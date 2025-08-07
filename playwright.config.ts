7e8c71cf6894fb949ba53197867459c7364eb975
souradeep10 <souradeep.ghosh10@gmail.com>
2025-08-07 18:10:51 +0530

    commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-  ['list'],
-  ['json', { outputFile: 'playwright-report/report.json' }],
-  ['html', { outputFolder: 'playwright-report/html-report', open: 'never' }]
-],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['json', { outputFile: 'playwright-report/report.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
