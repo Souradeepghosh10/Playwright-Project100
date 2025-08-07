ddad83dad1280595271f96760a92c28411378778
Souradeepghosh10 <44546427+Souradeepghosh10@users.noreply.github.com>
2025-08-07 20:35:43 +0530

    Update facebook.spec.ts

--- tests/facebook.spec.ts
+++ tests/facebook.spec.ts
@@ -5,7 +5,7 @@ test('End-to-end: Verify Facebook login page loads', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
 
   // Step 2: Verify Facebook login page is displayed
-  await expect(page).toHaveTitle(/Facebook/);
+  await expect(page).toHaveTitle(/Facebook1/);
   await expect(page.locator('input[name="email"]')).toBeVisible();
   await expect(page.locator('input[name="pass"]')).toBeVisible();
   await expect(page.getByRole('button', { name: /Log In/i })).toBeVisible();
